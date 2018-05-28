09f7fd3ebb9463f80aadf5c88bf8d1f92d927548
Jose Augusto <j.augustocss@gmail.com>
2018-05-28 18:05:09 -0300

    Atualização

--- app/src/main/java/enfermaria/projeto/com/enfermaria/CadatroPaciente.java
+++ app/src/main/java/enfermaria/projeto/com/enfermaria/CadatroPaciente.java
@@ -1,5 +1,6 @@
 package enfermaria.projeto.com.enfermaria;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +13,9 @@ public class CadatroPaciente extends AppCompatActivity {
     private Button botaoVoltar;
     private  Button botaoSalvar;
     private EditText textoNome;
+    private EditText textoLeito;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,7 @@ public class CadatroPaciente extends AppCompatActivity {
         botaoVoltar = findViewById(R.id.botaoVoltar_Id);
         botaoSalvar = findViewById(R.id.botaoSalvar_Id);
         textoNome = findViewById(R.id.editNome_Id);
+        textoLeito = findViewById(R.id.editLeito_Id);
 
         botaoVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,7 +37,12 @@ public class CadatroPaciente extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(CadatroPaciente.this, DadosPaciente.class);
 
-                intent.putExtra("nome","Nome do Paciente: " +textoNome.getText().toString() );
+                Bundle bundle =new Bundle();
+
+               bundle.putString("nome", textoNome.getText().toString());
+                bundle.putInt("leito", Integer.parseInt((textoLeito.getText().toString())));
+
+                intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
--- app/src/main/java/enfermaria/projeto/com/enfermaria/DadosPaciente.java
+++ app/src/main/java/enfermaria/projeto/com/enfermaria/DadosPaciente.java
@@ -1,22 +1,34 @@
 package enfermaria.projeto.com.enfermaria;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class DadosPaciente extends AppCompatActivity {
-    private TextView texto;
+    private TextView textoNome;
+    private TextView textoLeit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dados_paciente);
-        texto = findViewById(R.id.textNome_Id);
-        Bundle extra = getIntent().getExtras();
+        textoNome = findViewById(R.id.textNome_Id);
+        textoLeit = findViewById(R.id.textLeito_Id);
 
-        if(extra !=null){
-            String textoPassado = extra.getString("nome");
-            texto.setText(textoPassado);
+        Intent intent = getIntent();
+
+        Bundle params = intent.getExtras();
+
+
+        if(params!=null){
+           String texto = params.getString("nome");
+           textoNome.setText(texto);
+           int leito = params.getInt("leito");
+           textoLeit.setText(String.valueOf(leito));
+
+
         }
+
     }
 }
